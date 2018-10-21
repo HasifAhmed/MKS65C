3d9a9eaa32424c32a79bfcb92317d8c43675395c
hasifahmed <hahmed@stuy.edu>
2018-10-21 16:34:10 -0400

    basic sieve

--- makefile
+++ makefile
@@ -1,8 +1,14 @@
 all:sieve.o tester.o
-	gcc sieve.o tester.o
+	gcc sieve.o tester.o -lm
+
+run:
+	./a.out $(args)
 
 sieve.o: sieve.c sieve.h
 	gcc -c sieve.c
 
-tester.o: tester.c
+tester.o: tester.c sieve.h
 	gcc -c tester.c
+
+clean:
+	rm *.o
--- sieve.c
+++ sieve.c
@@ -1,4 +1,39 @@
 #include "sieve.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int seive(int targetPrime){
+int sieve(int targetPrime){
+    int range;
+
+    if(targetPrime > 5000){
+      range = targetPrime * log(targetPrime) * 1.15;
+    }
+    else{
+      range = targetPrime * log(targetPrime) * 1.3 + 10;
+    }
+    int * arr = calloc(sizeof(int),range);
+
+
+
+
+    int cnt = 0;
+    int i = 2;
+    int i2;
+    for(;i<range;i++){
+      if(arr[i] == 0){
+        cnt ++;
+
+        if(cnt == targetPrime){
+          return i;
+        }
+
+        i2 = i + i;
+        for(;i2 < range; i2 += i){
+          arr[i2] = 1;
+        }
+      }
+    }
+    return 0;
+
+
 }
